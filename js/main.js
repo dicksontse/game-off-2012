6e6eada114f2197ff655c6c797e378bc2e371c23
Dickson Tse <dtse87@gmail.com>
2012-11-30 21:02:35 -0800

    Create basic game loop

--- js/main.js
+++ js/main.js
@@ -1,3 +1,22 @@
 $(document).ready(function() {
-  
+  var snake = function() {
+    var canvas = $("#canvas")[0];
+    var w = canvas.width();
+    var h = canvas.height();
+    var draw = canvas.getContext('2d');
+
+    function tick() {
+      draw.clearRect(0, 0, w, h);
+
+      setTimeout(function() { tick() }, 100);
+    }
+
+    return {
+      init: function() {
+        tick();
+      }
+    }
+  };
+
+  snake.init();
 });
