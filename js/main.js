c95a26738abd8bb971b1c79c3563c8bf4dd62fcf
Dickson Tse <dtse87@gmail.com>
2012-11-30 21:12:43 -0800

    Draw and update the snake

--- js/main.js
+++ js/main.js
@@ -1,12 +1,29 @@
 $(document).ready(function() {
   var snake = function() {
-    var canvas = $("#canvas")[0];
+    var canvas = $("#canvas");
     var w = canvas.width();
     var h = canvas.height();
-    var draw = canvas.getContext('2d');
+    var draw = canvas[0].getContext('2d');
+    var unit = 40;
+    var position = [0, 0];
+
+    function updatePosition(current) {
+      current[0] += unit;
+      if (current[0] + unit > w) {
+        current[0] = 0;
+      }
+    }
+
+    function drawSnake(current) {
+      draw.fillStyle = "rgb(65, 131, 196)";
+      draw.fillRect(current[0], current[1], unit, unit);
+    }
 
     function tick() {
       draw.clearRect(0, 0, w, h);
+
+      updatePosition(position);
+      drawSnake(position);
 
       setTimeout(function() { tick() }, 100);
     }
@@ -16,7 +33,7 @@ $(document).ready(function() {
         tick();
       }
     }
-  };
+  } ();
 
   snake.init();
 });
